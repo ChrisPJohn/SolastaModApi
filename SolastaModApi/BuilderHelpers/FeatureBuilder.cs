826d92c85938771a191fe108109917b5d4efbe9d
leprechaun7377 <chris.p.john@gmail.com>
2020-12-06 01:47:25 -0800

    Add some helpers to make create classes, equipment options, features, and gui presentations easier.

---
+++ SolastaModApi/BuilderHelpers/FeatureBuilder.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SolastaModApi
+{
+    class FeatureBuilder
+    {
+        public static FeatureDefinitionProficiency BuildProficiency(RuleDefinitions.ProficiencyType type,
+            List<string> proficiencies, string name, GuiPresentation guiPresentation)
+        {
+            FeatureDefinitionProficiency proficiency = ScriptableObject.CreateInstance<FeatureDefinitionProficiency>();
+            Traverse.Create(proficiency).Field("proficiencyType").SetValue(type);
+            foreach(string item in proficiencies)
+            {
+                proficiency.Proficiencies.Add(item);
+            }
+            Traverse.Create(proficiency).Field("name").SetValue(name);
+            Traverse.Create(proficiency).Field("guiPresentation").SetValue(guiPresentation);
+            return proficiency;
+        }
+
+        public static FeatureDefinitionPointPool BuildPointPool(HeroDefinitions.PointsPoolType poolType, int poolAmount,
+            List<string> choices, string name, GuiPresentation guiPresentation)
+        {
+            FeatureDefinitionPointPool pointPool = ScriptableObject.CreateInstance<FeatureDefinitionPointPool>();
+            Traverse.Create(pointPool).Field("poolType").SetValue(poolType);
+            Traverse.Create(pointPool).Field("poolAmount").SetValue(poolAmount);
+            foreach (string item in choices)
+            {
+                pointPool.RestrictedChoices.Add(item);
+            }
+            Traverse.Create(pointPool).Field("name").SetValue(name);
+            Traverse.Create(pointPool).Field("guiPresentation").SetValue(guiPresentation);
+            return pointPool;
+        }
+    }
+}
---
+++ SolastaModApi/BuilderHelpers/GuiPresentationBuilder.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace SolastaModApi
+{
+    class GuiPresentationBuilder
+    {
+        GuiPresentation guiPresentation;
+
+        public GuiPresentationBuilder(string description, string title)
+        {
+            guiPresentation = new GuiPresentation();
+            guiPresentation.Description = description;
+            guiPresentation.Title = title;
+        }
+
+        public void SetColor(Color color)
+        {
+            Traverse.Create(guiPresentation).Field("color").SetValue(new Color(1.0f, 1.0f, 1.0f, 1.0f));
+        }
+
+        public void SetSortOrder(int sortOrder)
+        {
+            Traverse.Create(guiPresentation).Field("sortOrder").SetValue(1);
+        }
+
+        public void SetSpriteReference(AssetReferenceSprite sprite)
+        {
+            Traverse.Create(guiPresentation).Field("spriteReference").SetValue(sprite);
+        }
+
+        public GuiPresentation Build()
+        {
+            return guiPresentation;
+        }
+    }
+}

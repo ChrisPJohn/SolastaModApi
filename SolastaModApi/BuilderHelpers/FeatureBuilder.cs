8009b66bc574cc7e0fa32e6623a2807338515921
leprechaun7377 <chris.p.john@gmail.com>
2020-12-09 01:48:26 -0800

    Lots of magic affinity builders as well as attack modifier and an attribute modifier that takes a gui.

--- SolastaModApi/BuilderHelpers/FeatureBuilder.cs
+++ SolastaModApi/BuilderHelpers/FeatureBuilder.cs
@@ -60,6 +60,47 @@ namespace SolastaModApi
             return attributeMod;
         }
 
+        public static FeatureDefinitionAttributeModifier BuildAttributeModifier(AttributeModifierOperation modifierType,
+            string attribute, int amount, string name, GuiPresentation guiPresentation)
+        {
+            FeatureDefinitionAttributeModifier attributeMod = ScriptableObject.CreateInstance<FeatureDefinitionAttributeModifier>();
+            Traverse.Create(attributeMod).Field("modifierType2").SetValue(modifierType);
+            Traverse.Create(attributeMod).Field("modifiedAttribute").SetValue(attribute);
+            Traverse.Create(attributeMod).Field("modifierValue").SetValue(amount);
+            Traverse.Create(attributeMod).Field("name").SetValue(name);
+            attributeMod.name = name;
+            Traverse.Create(attributeMod).Field("guiPresentation").SetValue(guiPresentation);
+
+            Traverse.Create(attributeMod).Field("guid").SetValue(System.Guid.NewGuid().ToString());
+            DatabaseRepository.GetDatabase<FeatureDefinition>().Add(attributeMod);
+            return attributeMod;
+        }
+
+        public static FeatureDefinitionAttackModifier BuildAttackModifier(RuleDefinitions.AttackModifierMethod attackRollModifierMethod,
+            int attackRollModifier, string attackRollAbilityScore, RuleDefinitions.AttackModifierMethod damageRollModifierMethod,
+            int damageRollModifier, string damageRollAbilityScore, bool canAddAbilityBonusToSecondary, string additionalAttackTag,
+            string name, GuiPresentation guiPresentation)
+        {
+            FeatureDefinitionAttackModifier attackModifier = ScriptableObject.CreateInstance<FeatureDefinitionAttackModifier>();
+
+            Traverse.Create(attackModifier).Field("attackRollModifierMethod").SetValue(attackRollModifierMethod);
+            Traverse.Create(attackModifier).Field("attackRollModifier").SetValue(attackRollModifier);
+            Traverse.Create(attackModifier).Field("attackRollAbilityScore").SetValue(attackRollAbilityScore);
+            Traverse.Create(attackModifier).Field("damageRollModifierMethod").SetValue(damageRollModifierMethod);
+            Traverse.Create(attackModifier).Field("damageRollModifier").SetValue(damageRollModifier);
+            Traverse.Create(attackModifier).Field("damageRollAbilityScore").SetValue(damageRollAbilityScore);
+            Traverse.Create(attackModifier).Field("canAddAbilityBonusToSecondary").SetValue(canAddAbilityBonusToSecondary);
+            Traverse.Create(attackModifier).Field("additionalAttackTag").SetValue(additionalAttackTag);
+
+            Traverse.Create(attackModifier).Field("name").SetValue(name);
+            attackModifier.name = name;
+            Traverse.Create(attackModifier).Field("guiPresentation").SetValue(guiPresentation);
+
+            Traverse.Create(attackModifier).Field("guid").SetValue(System.Guid.NewGuid().ToString());
+            DatabaseRepository.GetDatabase<FeatureDefinition>().Add(attackModifier);
+            return attackModifier;
+        }
+
         public static FeatureDefinitionSavingThrowAffinity BuildSavingThrowAffinity(List<string> abilityScores,
             RuleDefinitions.CharacterSavingThrowAffinity affinityType, bool againstMagic, string name, GuiPresentation guiPresentation)
         {
@@ -115,6 +156,169 @@ namespace SolastaModApi
             Traverse.Create(abilityAffinity).Field("guid").SetValue(System.Guid.NewGuid().ToString());
             DatabaseRepository.GetDatabase<FeatureDefinition>().Add(abilityAffinity);
             return abilityAffinity;
+        }
+
+
+
+        public static FeatureDefinitionMagicAffinity BuildMagicAffinityConcentration(RuleDefinitions.AdvantageType advantageType, int threshold, string name, GuiPresentation guiPresentation)
+        {
+            FeatureDefinitionMagicAffinity magicAffinity = ScriptableObject.CreateInstance<FeatureDefinitionMagicAffinity>();
+
+            Traverse.Create(magicAffinity).Field("concentrationAdvantage").SetValue(advantageType);
+            if (threshold > 0)
+            {
+                Traverse.Create(magicAffinity).Field("overConcentrationThreshold").SetValue(threshold);
+            }
+
+            Traverse.Create(magicAffinity).Field("name").SetValue(name);
+            magicAffinity.name = name;
+            Traverse.Create(magicAffinity).Field("guiPresentation").SetValue(guiPresentation);
+            Traverse.Create(magicAffinity).Field("guid").SetValue(System.Guid.NewGuid().ToString());
+            DatabaseRepository.GetDatabase<FeatureDefinition>().Add(magicAffinity);
+            return magicAffinity;
+        }
+
+        public static FeatureDefinitionMagicAffinity BuildMagicAffinitySpecialtySpell(SpellDefinition spell, RuleDefinitions.AdvantageType advantageType, string name, GuiPresentation guiPresentation)
+        {
+            FeatureDefinitionMagicAffinity magicAffinity = ScriptableObject.CreateInstance<FeatureDefinitionMagicAffinity>();
+
+            Traverse.Create(magicAffinity).Field("forcedSpellDefinition").SetValue(spell);
+            Traverse.Create(magicAffinity).Field("forcedSavingThrowAffinity").SetValue(advantageType);
+
+            Traverse.Create(magicAffinity).Field("name").SetValue(name);
+            magicAffinity.name = name;
+            Traverse.Create(magicAffinity).Field("guiPresentation").SetValue(guiPresentation);
+            Traverse.Create(magicAffinity).Field("guid").SetValue(System.Guid.NewGuid().ToString());
+            DatabaseRepository.GetDatabase<FeatureDefinition>().Add(magicAffinity);
+            return magicAffinity;
+        }
+
+        public static FeatureDefinitionMagicAffinity BuildMagicAffinityModifiers(int attackModifier, int dcModifier, int preparedModifier, string name, GuiPresentation guiPresentation)
+        {
+            FeatureDefinitionMagicAffinity magicAffinity = ScriptableObject.CreateInstance<FeatureDefinitionMagicAffinity>();
+
+            if (attackModifier != 0)
+            {
+                Traverse.Create(magicAffinity).Field("spellAttackModifier").SetValue(attackModifier);
+            }
+            if (dcModifier != 0)
+            {
+                Traverse.Create(magicAffinity).Field("saveDCModifier").SetValue(dcModifier);
+            }
+            if (preparedModifier != 0)
+            {
+                Traverse.Create(magicAffinity).Field("maxPreparedSpellsModifier").SetValue(preparedModifier);
+            }
+
+            Traverse.Create(magicAffinity).Field("name").SetValue(name);
+            magicAffinity.name = name;
+            Traverse.Create(magicAffinity).Field("guiPresentation").SetValue(guiPresentation);
+            Traverse.Create(magicAffinity).Field("guid").SetValue(System.Guid.NewGuid().ToString());
+            DatabaseRepository.GetDatabase<FeatureDefinition>().Add(magicAffinity);
+            return magicAffinity;
+        }
+
+        public static FeatureDefinitionMagicAffinity BuildMagicAffinityScribing(float scribeDurationMultiplier, float scribeCostMultiplier,
+            int additionalScribedSpells, RuleDefinitions.AdvantageType scribeAdvantage, string name, GuiPresentation guiPresentation)
+        {
+            FeatureDefinitionMagicAffinity magicAffinity = ScriptableObject.CreateInstance<FeatureDefinitionMagicAffinity>();
+
+            Traverse.Create(magicAffinity).Field("scribeDurationMultiplier").SetValue(scribeDurationMultiplier);
+            Traverse.Create(magicAffinity).Field("scribeCostMultiplier").SetValue(scribeCostMultiplier);
+            Traverse.Create(magicAffinity).Field("additionalScribedSpells").SetValue(additionalScribedSpells);
+            Traverse.Create(magicAffinity).Field("scribeAdvantageType").SetValue(scribeAdvantage);
+
+            Traverse.Create(magicAffinity).Field("name").SetValue(name);
+            magicAffinity.name = name;
+            Traverse.Create(magicAffinity).Field("guiPresentation").SetValue(guiPresentation);
+            Traverse.Create(magicAffinity).Field("guid").SetValue(System.Guid.NewGuid().ToString());
+            DatabaseRepository.GetDatabase<FeatureDefinition>().Add(magicAffinity);
+            return magicAffinity;
+        }
+
+        // ????
+        //public bool CanFailSpellcasting { get; }
+        //public int SpellcastingSuccessDC { get; }
+
+        public static FeatureDefinitionMagicAffinity BuildMagicAffinityHeightenedList(List<string> spellNames, int levelBonus, string name, GuiPresentation guiPresentation)
+        {
+            FeatureDefinitionMagicAffinity magicAffinity = ScriptableObject.CreateInstance<FeatureDefinitionMagicAffinity>();
+
+            Traverse.Create(magicAffinity).Field("warListSlotBonus").SetValue(levelBonus);
+            Traverse.Create(magicAffinity).Field("UsesWarList").SetValue(true);
+            foreach (string spell in spellNames)
+            {
+                magicAffinity.WarListSpells.Add(spell);
+            }
+
+            Traverse.Create(magicAffinity).Field("name").SetValue(name);
+            magicAffinity.name = name;
+            Traverse.Create(magicAffinity).Field("guiPresentation").SetValue(guiPresentation);
+            Traverse.Create(magicAffinity).Field("guid").SetValue(System.Guid.NewGuid().ToString());
+            DatabaseRepository.GetDatabase<FeatureDefinition>().Add(magicAffinity);
+            return magicAffinity;
+        }
+
+        public static FeatureDefinitionMagicAffinity BuildMagicAffinityRitualCasting(RuleDefinitions.RitualCasting ritualCasting, string name, GuiPresentation guiPresentation)
+        {
+            FeatureDefinitionMagicAffinity magicAffinity = ScriptableObject.CreateInstance<FeatureDefinitionMagicAffinity>();
+
+            Traverse.Create(magicAffinity).Field("ritualCasting").SetValue(ritualCasting);
+
+            Traverse.Create(magicAffinity).Field("name").SetValue(name);
+            magicAffinity.name = name;
+            Traverse.Create(magicAffinity).Field("guiPresentation").SetValue(guiPresentation);
+            Traverse.Create(magicAffinity).Field("guid").SetValue(System.Guid.NewGuid().ToString());
+            DatabaseRepository.GetDatabase<FeatureDefinition>().Add(magicAffinity);
+            return magicAffinity;
+        }
+
+        public static FeatureDefinitionMagicAffinity BuildMagicAffinityExtendedSpellList(SpellListDefinition spellListDefinition, string name, GuiPresentation guiPresentation)
+        {
+            FeatureDefinitionMagicAffinity magicAffinity = ScriptableObject.CreateInstance<FeatureDefinitionMagicAffinity>();
+
+            Traverse.Create(magicAffinity).Field("extendedSpellList").SetValue(spellListDefinition);
+
+            Traverse.Create(magicAffinity).Field("name").SetValue(name);
+            magicAffinity.name = name;
+            Traverse.Create(magicAffinity).Field("guiPresentation").SetValue(guiPresentation);
+            Traverse.Create(magicAffinity).Field("guid").SetValue(System.Guid.NewGuid().ToString());
+            DatabaseRepository.GetDatabase<FeatureDefinition>().Add(magicAffinity);
+            return magicAffinity;
+        }
+
+        public static FeatureDefinitionMagicAffinity BuildMagicAffinityImmunities(List<string> spellNames, string name, GuiPresentation guiPresentation)
+        {
+            FeatureDefinitionMagicAffinity magicAffinity = ScriptableObject.CreateInstance<FeatureDefinitionMagicAffinity>();
+
+            foreach (string spell in spellNames)
+            {
+                magicAffinity.SpellImmunities.Add(spell);
+            }
+
+            Traverse.Create(magicAffinity).Field("name").SetValue(name);
+            magicAffinity.name = name;
+            Traverse.Create(magicAffinity).Field("guiPresentation").SetValue(guiPresentation);
+            Traverse.Create(magicAffinity).Field("guid").SetValue(System.Guid.NewGuid().ToString());
+            DatabaseRepository.GetDatabase<FeatureDefinition>().Add(magicAffinity);
+            return magicAffinity;
+        }
+
+        public static FeatureDefinitionMagicAffinity BuildMagicAffinityAutoIdentify(List<string> autoIdentify, string name, GuiPresentation guiPresentation)
+        {
+            FeatureDefinitionMagicAffinity magicAffinity = ScriptableObject.CreateInstance<FeatureDefinitionMagicAffinity>();
+
+            foreach (string tag in autoIdentify)
+            {
+                magicAffinity.DeviceTagsAutoIdentifying.Add(tag);
+            }
+
+            Traverse.Create(magicAffinity).Field("name").SetValue(name);
+            magicAffinity.name = name;
+            Traverse.Create(magicAffinity).Field("guiPresentation").SetValue(guiPresentation);
+            Traverse.Create(magicAffinity).Field("guid").SetValue(System.Guid.NewGuid().ToString());
+            DatabaseRepository.GetDatabase<FeatureDefinition>().Add(magicAffinity);
+            return magicAffinity;
         }
     }
 }

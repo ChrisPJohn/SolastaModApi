7cec66f440348365482dc1547fc87ee0509dacf1
leprechaun7377 <chris.p.john@gmail.com>
2021-03-28 02:55:33 -0700

    Add some useful functionality to main to help mods get started.
    Also add several helper classes that make creating various DB entries easier.
    
    (cherry picked from commit 2b111bb8960b0dcdbdca30069d99cc38e18ec620)

---
+++ SolastaModApi/BuilderHelpers/EquipmentOptionsBuilder.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using static CharacterClassDefinition;
+
+namespace SolastaModApi
+{
+    class EquipmentOptionsBuilder
+    {
+        public static HeroEquipmentOption Option(ItemDefinition itemType, string optionType, int number)
+        {
+            HeroEquipmentOption itemOption = new HeroEquipmentOption();
+            Traverse.Create(itemOption).Field("number").SetValue(number);
+            Traverse.Create(itemOption).Field("optionType").SetValue(optionType);
+            Traverse.Create(itemOption).Field("itemReference").SetValue(itemType);
+            return itemOption;
+        }
+
+        public static HeroEquipmentOption Option(string defaultChoice, string optionType, int number)
+        {
+            HeroEquipmentOption itemOption = new HeroEquipmentOption();
+            Traverse.Create(itemOption).Field("number").SetValue(number);
+            Traverse.Create(itemOption).Field("optionType").SetValue(optionType);
+            Traverse.Create(itemOption).Field("defaultChoice").SetValue(defaultChoice);
+            return itemOption;
+        }
+
+
+    }
+}

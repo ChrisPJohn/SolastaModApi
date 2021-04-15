5052e3d804dd54999d0b76a487f077a72d325918
ThyWoof <pauloesquilo@gmail.com>
2021-04-14 18:53:57 -0700

    fix typo

--- SolastaModApi/BuilderHelpers/EquipmentOptionsBuilder.cs
+++ SolastaModApi/BuilderHelpers/EquipmentOptionsBuilder.cs
@@ -3,7 +3,7 @@ using static CharacterClassDefinition;
 
 namespace SolastaModApi
 {
-    pubic class EquipmentOptionsBuilder
+    public class EquipmentOptionsBuilder
     {
         public static HeroEquipmentOption Option(ItemDefinition itemType, string optionType, int number)
         {

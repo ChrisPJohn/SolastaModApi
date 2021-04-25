c870185f2c02e9b13cf4cf324f0fe6e138221b50
Philip Lee <github@pjlconsultants.co.uk>
2021-04-25 16:34:16 +0100

    Add some helper classes

---
+++ SolastaModApi/BuilderHelpers/BaseDefinitionBuilder.cs
@@ -0,0 +1,86 @@
+﻿using SolastaModApi.Diagnostics;
+using SolastaModApi.Infrastructure;
+using SolastaTesting.SolastaModApi;
+using UnityEngine;
+
+namespace SolastaModApi.BuilderHelpers
+{
+    /// <summary>
+    ///     Base class builder for all classes derived from BaseDefinition
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class BaseDefinitionBuilder<T> where T : BaseDefinition
+    {
+        /// <summary>
+        /// Create a new one
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="guid"></param>
+        protected BaseDefinitionBuilder(string name, string guid)
+        {
+            Definition = ScriptableObject.CreateInstance<T>();
+
+            Definition.name = name;
+            Definition.SetField("guid", guid);
+        }
+
+        /// <summary>
+        /// Create clone and rename
+        /// </summary>
+        /// <param name="original"></param>
+        /// <param name="name"></param>
+        /// <param name="guid"></param>
+        protected BaseDefinitionBuilder(T original, string name, string guid)
+        {
+            Definition = Object.Instantiate(original);
+
+            Definition.name = name;
+            Definition.SetField("guid", guid);
+        }
+
+        /// <summary>
+        /// Take ownership
+        /// </summary>
+        /// <param name="original"></param>
+        protected BaseDefinitionBuilder(T original)
+        {
+            Definition = original;
+        }
+
+        private static void AddToDB<TDb>(TDb definition, bool assertIfDuplicate = true) where TDb : BaseDefinition
+        {
+            var db = DatabaseRepository.GetDatabase<TDb>();
+
+            Assert.IsNotNull(db, $"Database '{typeof(TDb).Name}' not found.");
+
+            if (assertIfDuplicate && (db.HasElement(definition.name) || db.HasElementByGuid(definition.GUID)))
+                throw new SolastaModApiException(
+                    $"The definition with name '{definition.name}' already exists in database '{typeof(TDb)}'");
+
+            db.Add(definition);
+        }
+
+        public T AddToDB(bool assertIfDuplicate = true)
+        {
+            var fd = Definition as FeatureDefinition;
+
+            if (fd != null)
+            {
+                AddToDB(fd, assertIfDuplicate);
+            }
+            else
+            {
+                AddToDB(Definition, assertIfDuplicate);
+            }
+
+            return Definition;
+        }
+
+        public void SetGuiPresentation(GuiPresentation guiPresentation)
+        {
+            Definition.SetField("guiPresentation", guiPresentation);
+        }
+
+        protected T Definition { get; }
+    }
+}
--- SolastaModApi/Main.cs
+++ SolastaModApi/Main.cs
@@ -1,10 +1,22 @@
-﻿namespace SolastaModApi
-{
-    public class Main
-    {
-        public static void Load()
-        {
+﻿using System;
+using System.Diagnostics;
+using UnityModManagerNet;
 
+namespace SolastaModApi
+{
+    public static class Main
+    {
+        [Conditional("DEBUG")]
+        internal static void Log(string msg) => Logger.Log(msg);
+
+        internal static void Error(Exception ex) => Logger?.Error(ex.ToString());
+        internal static void Error(string msg) => Logger?.Error(msg);
+        internal static UnityModManager.ModEntry.ModLogger Logger { get; private set; }
+
+        internal static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            Logger = modEntry.Logger;
+            return true;
         }
     }
 }

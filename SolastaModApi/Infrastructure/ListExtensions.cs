c4ab2d1bc27adc22d3cdd116a27aa292cc9e2959
Philip Lee <github@pjlconsultants.co.uk>
2021-05-31 09:12:42 +0100

    Add repository.json and helper util

---
+++ SolastaModApi/Infrastructure/ListExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace SolastaModApi.Infrastructure
+{
+    public static class ListExtensions
+    {
+        public static void AddRangeEx<T>(this List<T> list, params T[] range)
+        {
+            list.AddRange(range);
+        }
+    }
+}

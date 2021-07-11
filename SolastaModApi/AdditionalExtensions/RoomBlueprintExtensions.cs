bd11e03df0f2cf4ee3bec62c48021e0f38a0c154
Philip Lee <github@pjlconsultants.co.uk>
2021-07-11 09:47:01 +0100

    Add map from SetMinLevel to SetMinStartLevel SetMaxLevel to SetMaxStartLevel

--- SolastaModApi/AdditionalExtensions/RoomBlueprintExtensions.cs
+++ SolastaModApi/AdditionalExtensions/RoomBlueprintExtensions.cs
@@ -1,9 +1,11 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace SolastaModApi.Extensions
 {
     public static partial class RoomBlueprintExtensions
     {
+        [Obsolete("Use SetGroundMaskSprite")]
         public static T SetGrounMaskSprite<T>(this T entity, Sprite value)
             where T : RoomBlueprint
         {

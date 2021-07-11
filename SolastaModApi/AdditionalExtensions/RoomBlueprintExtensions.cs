1dd85efb575296f68a9427ab98714b7267427cbe
Philip Lee <github@pjlconsultants.co.uk>
2021-07-11 09:41:46 +0100

    Add mapping for SetGrounMaskSprite to SetGroundMaskSprite

---
+++ SolastaModApi/AdditionalExtensions/RoomBlueprintExtensions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace SolastaModApi.Extensions
+{
+    public static partial class RoomBlueprintExtensions
+    {
+        public static T SetGrounMaskSprite<T>(this T entity, Sprite value)
+            where T : RoomBlueprint
+        {
+            entity.SetGroundMaskSprite(value);
+            return entity;
+        }
+    }
+}
